09e73abb19a3cfecb11fcd3d76da82749ef7131c
surya <bittergourd76@gmail.com>
2020-10-18 07:56:42 +0530

    modified handle message

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import { response } from 'express';
 import request from 'request';
 
 const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
@@ -69,49 +70,49 @@ let postWebhook = (req, res) => {
 };
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
-  let response;
+// function handleMessage(sender_psid, received_message) {
+//   let response;
   
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
-  } 
+//   // Checks if the message contains text
+//   if (received_message.text) {    
+//     // Create the payload for a basic text message, which
+//     // will be added to the body of our request to the Send API
+//     response = {
+//       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+//     }
+//   } else if (received_message.attachments) {
+//     // Get the URL of the message attachment
+//     let attachment_url = received_message.attachments[0].payload.url;
+//     response = {
+//       "attachment": {
+//         "type": "template",
+//         "payload": {
+//           "template_type": "generic",
+//           "elements": [{
+//             "title": "Is this the right picture?",
+//             "subtitle": "Tap a button to answer.",
+//             "image_url": attachment_url,
+//             "buttons": [
+//               {
+//                 "type": "postback",
+//                 "title": "Yes!",
+//                 "payload": "yes",
+//               },
+//               {
+//                 "type": "postback",
+//                 "title": "No!",
+//                 "payload": "no",
+//               }
+//             ],
+//           }]
+//         }
+//       }
+//     }
+//   } 
   
-  // Send the response message
-  callSendAPI(sender_psid, response);    
-}
+//   // Send the response message
+//   callSendAPI(sender_psid, response);    
+// }
 
 
 
@@ -141,7 +142,7 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
-    "message": response
+    "message": {"text": response}
   }
 
   // Send the HTTP request to the Messenger Platform
@@ -157,6 +158,22 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   }); 
+}
+
+function firstTrait(nlp, name) {
+  return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
+}
+
+function handleMessage(sender_psid, message) {
+  // check greeting is here and is confident
+  const greeting = firstTrait(message.nlp, 'wit$greetings');
+  if (greeting && greeting.confidence > 0.8) {
+    //sendResponse('Hi there!');
+    callSendAPI(sender_psid, {response: 'Hi there!'});
+  } else { 
+    // default logic
+    callSendAPI(sender_psid, {response: 'default'});
+  }
 }
 
 module.exports = {
