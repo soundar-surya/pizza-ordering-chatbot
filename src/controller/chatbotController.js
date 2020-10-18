05f59dd69eee30cfc18a804e65444e2b47d34fcc
surya <bittergourd76@gmail.com>
2020-10-18 20:04:31 +0530

    modified rsponse

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -132,7 +132,37 @@ const handlePostback = (sender_psid, received_postback) => {
       response = { "text": "reg...." }
   else if(payload === 'medium') response = {"text": "med...."}
   else if (payload === 'veg')
-      response = {"text": "Pick One"}
+      response = {
+                "attachment":{
+                  "type":"template",
+                  "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                      {
+                        "title":"Pick One!",
+                        "image_url":"https://petersfancybrownhats.com/company_image.png",
+                        "subtitle":"We have the right hat for everyone.",
+                        "default_action": {
+                          "type": "web_url",
+                          "url": "https://petersfancybrownhats.com/view?item=103",
+                          "webview_height_ratio": "tall",
+                        },
+                        "buttons":[
+                          {
+                            "type":"web_url",
+                            "url":"https://petersfancybrownhats.com",
+                            "title":"View Website"
+                          },{
+                            "type":"postback",
+                            "title":"Start Chatting",
+                            "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                          }              
+                        ]      
+                      }
+                    ]
+                  }
+                }
+      }
 
   else if (payload === 'Non-veg')
       response = { "text": "Pick One" }
