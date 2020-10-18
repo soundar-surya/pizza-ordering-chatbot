c46c2cd547aedac6521e86438fd8195ec37b84b4
surya <bittergourd76@gmail.com>
2020-10-18 11:17:47 +0530

    added styles

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 let configViewEngine = app => {
     app.use(express.static("./src/public"));
+    app.use(express.static("./src/views"));
     app.set("view engine", "ejs");
     app.set("views", "./src/views");
 };
