a4584e011df59322def0faea52d673c99a3dad72
surya <bittergourd76@gmail.com>
2020-10-18 17:54:27 +0530

    modified rsponse

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -177,8 +177,7 @@ const handleMessage = (sender_psid, message) => {
   //handle message for react, like press like button
   // id like button: sticker_id 369239263222822
   if( message && message.attachments && message.attachments[0].payload){
-      callSendAPI(sender_psid, "Thanks. I had a great time with you.");
-      callSendAPIWithTemplate(sender_psid);
+      callSendAPI(sender_psid, "wow! That was epic.");
       return;
   }
 
@@ -204,6 +203,7 @@ const handleMessage = (sender_psid, message) => {
                             "payload": {
                               "template_type": "button",
                               "text": "Hi there! are you hungry? Let's get you something tasty delivered from Yo Yo Pizza.What kind of pizza do you want?",
+                              
                                 "buttons": [
                                   {
                                     "type": "postback",
@@ -215,10 +215,6 @@ const handleMessage = (sender_psid, message) => {
                                     "title": "Non-veg",
                                     "payload": "Non-veg",
                                   },
-                                  {
-                                    "type": "postback",
-                                    "payload": "yes",
-                                  }
                                 ],
                     
                             }
@@ -237,51 +233,6 @@ const handleMessage = (sender_psid, message) => {
       }
   }
 }
-
-let callSendAPIWithTemplate = (sender_psid) => {
-  // document fb message template
-  // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
-  let body = {
-      "recipient": {
-          "id": sender_psid
-      },
-      "message": {
-          "attachment": {
-              "type": "template",
-              "payload": {
-                  "template_type": "generic",
-                  "elements": [
-                      {
-                          "title": "Want to build sth awesome?",
-                          "image_url": "https://www.nexmo.com/wp-content/uploads/2018/10/build-bot-messages-api-768x384.png",
-                          "subtitle": "Watch more videos on my youtube channel ^^",
-                          "buttons": [
-                              {
-                                  "type": "web_url",
-                                  "url": "https://bit.ly/subscribe-haryphamdev",
-                                  "title": "Watch now"
-                              }
-                          ]
-                      }
-                  ]
-              }
-          }
-      }
-  };
-
-  request({
-      "uri": "https://graph.facebook.com/v6.0/me/messages",
-      "qs": { "access_token": process.env.FB_PAGE_TOKEN },
-      "method": "POST",
-      "json": body
-  }, (err, res, body) => {
-      if (!err) {
-          // console.log('message sent!')
-      } else {
-          console.error("Unable to send message:" + err);
-      }
-  });
-};
 
 
 module.exports = {
