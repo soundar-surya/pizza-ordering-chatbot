cc8dafbcafe3b31f52e79e1efba7c59360723ae1
surya <bittergourd76@gmail.com>
2020-10-18 16:55:46 +0530

    updated postback

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -56,10 +56,10 @@ const postWebhook = (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        console.log("qw"+webhook_event.message.text);
+        console.log(webhook_event.message.text);
         handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
-        console.log("qww"+webhook_event.postback);
+        console.log(webhook_event.postback);
         handlePostback(sender_psid, webhook_event.postback);
       }
     });
