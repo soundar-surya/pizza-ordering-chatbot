edd37e8375e8f07c9b91855034fe94fdf8cc9fa7
surya <bittergourd76@gmail.com>
2020-10-18 13:23:49 +0530

    updatd hooks

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -170,7 +170,6 @@ function firstTrait(nlp, name) {
 function handleMessage(sender_psid, message) {
   //handle message for react, like press like button
   // id like button: sticker_id 369239263222822
-  message.forEach(t => console.log(t));
   if( message && message.attachments && message.attachments[0].payload){
       callSendAPI(sender_psid, "Thanks. I had a great time with you.");
       callSendAPIWithTemplate(sender_psid);
