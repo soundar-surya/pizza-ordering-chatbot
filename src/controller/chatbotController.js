e56afc5c6274f3953dd07a1ef41b3651d9f5cf84
surya <bittergourd76@gmail.com>
2020-10-18 13:28:05 +0530

    updatd hooks

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -45,7 +45,8 @@ let postWebhook = (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event.text);
+      console.log(webhook_event);
+      webhook_event.map(({text}) => text);
 
     // Get the sender PSID
     let sender_psid = webhook_event.sender.id;
